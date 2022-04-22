8619611716f951c26291bdf33d9dbb7e944aa97d
Ryan Irelan <ryan@mijingo.com>
2022-04-21 22:48:30 -0500

    updated for build process

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/trail-quest-templates/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        aentry: resolve(__dirname, 'adventure-entry.html'),
+        alisting: resolve(__dirname, 'adventures-listing.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        content: resolve(__dirname, 'content-page.html'),
+      }
+    }
+  }
 })
